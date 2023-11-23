ee6e5dec166fe8dca50bab237a1d59fc220b48ec
RolandoCN <57764933+RolandoCN@users.noreply.github.com>
2023-11-22 20:40:57 -0500

    Se agrega funcion de potencia (#10)
    Co-authored-by: RolandoCN <juan.roland@outllok.es>

--- src/app/numbers/numbers.component.spec.ts
+++ src/app/numbers/numbers.component.spec.ts
@@ -1,4 +1,4 @@
-import { split, multiply } from './numbers.component';
+import { split, multiply, power } from './numbers.component';
 
 it(`This function divides two numbers`, () => {
   expect(split(6, 2)).toEqual(3);
@@ -8,4 +8,7 @@ it('This function multiply two numbers', () => {
   expect(multiply(1, 1)).toEqual(1);
 });
 
+it(`This function calculates the power of a number`, () => {
+  expect(power(2, 4)).toEqual(16);
+});
 
--- src/app/numbers/numbers.component.ts
+++ src/app/numbers/numbers.component.ts
@@ -18,3 +18,6 @@ export function multiply(num1: number, num2: number) {
   return num1 * num2;
 }
 
+export function power(base: number, exponente: number) {
+  return Math.pow(base, exponente);
+}
