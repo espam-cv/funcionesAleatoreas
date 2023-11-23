938a6a6ae6ea16270ba6b42576ec3ffed227f3d0
Gary Vera <garyveralucas@gmail.com>
2023-11-22 20:21:39 -0500

    Se agrega funcion multiplicar

--- src/app/numbers/numbers.component.spec.ts
+++ src/app/numbers/numbers.component.spec.ts
@@ -1,5 +1,11 @@
-import { split } from './numbers.component';
+import { split, multiply } from './numbers.component';
 
 it(`This function divides two numbers`, () => {
   expect(split(6, 2)).toEqual(3);
 });
+
+it('This function multiply two numbers', () => {
+  expect(multiply(1, 1)).toEqual(1);
+});
+
+
